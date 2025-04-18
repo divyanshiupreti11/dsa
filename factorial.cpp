0159a421b5082781cec5cd52ab003f46d99c62e4
divyanshiupreti11 <upretidivyanshi@gmail.com>
2025-04-18 18:33:17 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if (n==0){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+int main(){
+    int n;
+    cout<<"enter n:"<<endl;
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
