4a0e411b5adbea088ac035414f6d8d0da3f5846f
divyanshiupreti11 <upretidivyanshi@gmail.com>
2026-04-05 09:47:37 +0530

    Implement solution to find integers as sums of two cubes

---
+++ Integers With Multiple Sum of Two Cubes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findGoodIntegers(int n) {
+        unordered_map<long long ,int> counts;
+        for(long long a=1;a*a*a<n;++a){
+            long long a3=a*a*a;
+            for(long long b=a+1;;++b){
+                long long b3=b*b*b;
+                long long sum=a3+b3;
+                if(sum>n) break;
+                counts[sum]++;
+            }
+        }
+        vector<int> result;
+        for(auto const& [val,count]:counts){
+            if(count>=2){
+                result.push_back((int )val);
+            }
+        }
+        sort(result.begin(),result.end());
+        return result;
+    }
+};©leetcode
