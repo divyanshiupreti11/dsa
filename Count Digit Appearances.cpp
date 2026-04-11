a4c13aef3771686e8fb97a33c74321b2f5515087
divyanshiupreti11 <upretidivyanshi@gmail.com>
2026-04-11 23:04:55 +0530

    Create Count Digit Appearances.cpp

---
+++ Count Digit Appearances.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countDigitOccurrences(vector<int>& nums, int digit) {
+        int count=0;
+        for(int num:nums){
+            while(num>0){
+                if(num%10==digit) count++;
+                num=num/10;
+            }
+        }
+        return count;
+    }
+};
